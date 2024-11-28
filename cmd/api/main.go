148fa6dd1393e79c7fb1aeb1b1d091ad3ca1b835
pedrosfaria2 <pedroserrano2@gmail.com>
2024-11-27 21:23:03 -0300

    ✨ feat: add main entry point with graceful shutdown

---
+++ cmd/api/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	<-ctx.Done()
+
+	log.Println("Shutting down gracefully...")
+}
